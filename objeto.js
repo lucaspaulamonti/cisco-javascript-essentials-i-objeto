516c7a9f2097ca648cca82f09a9e45adda9e45f6
lucaspaulamonti <lucasdepaulamonti@gmail.com>
2022-10-18 00:10:23 -0300

    Create objeto.js

---
+++ objeto.js
@@ -0,0 +1,8 @@
+// Criando um objeto e imprimindo seus atributos.
+let testObj = {
+    nr: 600,
+    str: "text"
+};
+console.log(typeof testObj);
+console.log(testObj.nr);
+console.log(testObj.str);
